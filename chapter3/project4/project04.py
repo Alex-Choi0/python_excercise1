5df07b445de323f46e58abad93b250becdad50a1
Alex <choijeaho86@gmail.com>
2024-04-10 16:59:46 +0900

    chapter3 - project04 QR코드 생성기

---
+++ chapter3/project4/project04.py
@@ -0,0 +1,27 @@
+#Project04 QR코드 생성기
+
+# pip install qrcode
+
+import qrcode
+
+import os
+
+# 경로를 .py파일의 실행경로로 이동, 현재 경로로 이동
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+file_path = r'qr코드모음.txt'
+with open(file_path, 'rt', encoding='UTF8') as f :
+  read_lines = f.readlines()
+
+  for line in read_lines:
+    line = line.strip()
+    print(line)
+    qr_data = line
+    qr_img = qrcode.make(qr_data)
+    save_path = qr_data + '.png'
+    qr_img.save(save_path)
+
+# qr_data = 'www.naver.com'
+# qr_img = qrcode.make(qr_data)
+
+# save_path = qr_data + '.png'
+# qr_img.save(save_path)
